b0a75a7eb07ec83bbe5f343e685e940e9f8daf71
Rebeca <inscripciones225@gmail.com>
2025-05-08 00:21:27 -0400

    payment middlewares updates

--- src/routes/payments.routes.js
+++ src/routes/payments.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { createPayment } from "../controllers/payment.controller.js";
 
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { verifyAdmin } from "../middlewares/verifyAdmin.middleware.js";
+
 const router = Router();
 
-router.post("/mercadopago", createPayment);
+router.post("/mercadopago", authMiddleware, verifyAdmin, createPayment);
 
 export default router;
