7c0c668c1ef5c5619d08005df51930ff4b616a17
Rebeca <inscripciones225@gmail.com>
2025-05-18 08:42:39 -0400

    product model update

--- src/models/Product.model.js
+++ src/models/Product.model.js
@@ -8,6 +8,11 @@ const productSchema = new Schema(
     autores: { type: String, required: true },
     cd: { type: String, required: true },
     precio: { type: Types.Double, required: true },
+    imagen: { type: String, required: true },
+    anho_lanzamiento: { type: Number, required: true },
+    apple: { type: String, required: true },
+    spotify: { type: String, required: true },
+    youtube: { type: String, required: true },
     isActive: { type: Boolean, default: true },
   },
   {
